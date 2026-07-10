4362f889201ae2a0bf5983243999fc3b97d93448
Florian KOVACEVIC <florian.kovacevic00@gmail.com>
2026-07-10 22:52:47 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,7 +4,7 @@ Apex Markets — Application d'Analyse Financière
 Auteur  : FloKov
 Stack   : Streamlit · yfinance · Pandas · NumPy · Plotly
 Usage   : streamlit run app2.py
-"""
+""" 
 
 import json
 import warnings
